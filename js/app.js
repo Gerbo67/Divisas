b6b0c58881045e7b44e9dc85d0f7dacb74a5e805
Jose Fernando Resendiz Lopez <56377740+Gerbo67@users.noreply.github.com>
2022-01-13 12:08:18 -0600

    Image Style upload

--- js/app.js
+++ js/app.js
@@ -1,61 +1,84 @@
-async function validate() {
-    //Elements
-    const origin = document.getElementById('money-o').value;
-    const convert = document.getElementById('money-c').value;
-    const coinTo = document.getElementById('coinTo').value;
+window.onload=function(){
+    // Declare init HTML elements
+    const camera = document.querySelector('#camera');
+    const photo = document.querySelector('#photo');
+    const open = document.querySelector('#open');
 
-    //Validate not equals select
-    if (origin != convert) {
+    // Event to active input type file
+    open.addEventListener('click', function () {
+        camera.click();
+    });
 
-        const originF = valueFormat(origin);
-        const convertF = valueFormat(convert);
+    // Event on change content type file
+    camera.addEventListener('change', function (e) {
+        // Create url object and show Photo from BLOB Object.
+        photo.src = URL.createObjectURL(e.target.files[0]);
+    })
+}
 
-        if (originF != 'ERROR' && convertF != 'ERROR') {
+    async function validate() {
+        //Elements
+        const origin = document.getElementById('money-o').value;
+        const convert = document.getElementById('money-c').value;
+        const coinTo = document.getElementById('coinTo').value;
 
-            //URL
-            const url = `https://xecdapi.xe.com/v1/convert_to.json/?to=${originF}&from=${convertF}&amount=${coinTo}`;
+        //Validate not equals select
+        if (origin != convert) {
 
-            const xhr = new XMLHttpRequest();
-            xhr.open("GET", url);
+            const originF = valueFormat(origin);
+            const convertF = valueFormat(convert);
 
-            //Auth
-            xhr.setRequestHeader("Authorization", "Basic dW5pdmVyc2lkYWR0ZWNub2xvZ2ljYWRlbG5vcnRlZGVndWFuYWp1YXRvMTE0NzIxMzQ3OmdoczNzbTNrZTFqZG92NnRzM212ZzdmN2gy");
+            if (originF != 'ERROR' && convertF != 'ERROR') {
 
-            let jsonRes;
-            xhr.onreadystatechange = async function () {
-                if (xhr.readyState === 4) {
-                    jsonRes = await JSON.parse(xhr.responseText);
-                    console.log(jsonRes);
-                    document.getElementById('coinFrom').value = jsonRes.from[0].mid;
+                //validate coin null
+                if(coinTo>0){
+                    //URL
+                    const url = `https://xecdapi.xe.com/v1/convert_to.json/?to=${originF}&from=${convertF}&amount=${coinTo}`;
+
+                    const xhr = new XMLHttpRequest();
+                    xhr.open("GET", url);
+
+                    //Auth
+                    xhr.setRequestHeader("Authorization", "Basic dW5pdmVyc2lkYWR0ZWNub2xvZ2ljYWRlbG5vcnRlZGVndWFuYWp1YXRvMTE0NzIxMzQ3OmdoczNzbTNrZTFqZG92NnRzM212ZzdmN2gy");
+
+                    let jsonRes;
+                    xhr.onreadystatechange = async function () {
+                        if (xhr.readyState === 4) {
+                            jsonRes = await JSON.parse(xhr.responseText);
+                            console.log(jsonRes);
+                            document.getElementById('coinFrom').value = jsonRes.from[0].mid;
+                        }
+                    };
+
+                    xhr.send();
+                }else{
+                    alert("No hay cantidad");
                 }
-            };
 
-            xhr.send();
+            } else {
+                alert("Uso inadecuado");
+            }
 
         } else {
-            alert("Uso inadecuado");
+            alert("Conversion inecesaria");
         }
-
-    } else {
-        alert("Conversion inecesaria");
     }
-}
 
-function valueFormat(value) {
-    switch (value) {
-        case '1':
-            return 'MXN';
-            break;
-        case '2':
-            return 'EUR';
-            break;
-        case '3':
-            return 'GBP';
-            break;
-        case '4':
-            return 'USD';
-            break;
-        default:
-            return 'ERROR';
+    function valueFormat(value) {
+        switch (value) {
+            case '1':
+                return 'MXN';
+                break;
+            case '2':
+                return 'EUR';
+                break;
+            case '3':
+                return 'GBP';
+                break;
+            case '4':
+                return 'USD';
+                break;
+            default:
+                return 'ERROR';
+        }
     }
-}
