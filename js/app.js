6d5f259c0ac69c14619bcb91927640b435e5d362
Jose Fernando Resendiz Lopez <56377740+Gerbo67@users.noreply.github.com>
2022-01-12 22:57:20 -0600

    Fix 3

--- js/app.js
+++ js/app.js
@@ -1,15 +1,37 @@
 async function validate() {
+    //Elements
     const origin = document.getElementById('money-o').value;
     const convert = document.getElementById('money-c').value;
+    const coinTo = document.getElementById('coinTo').value;
 
+    //Validate not equals select
     if (origin != convert) {
 
         const originF = valueFormat(origin);
         const convertF = valueFormat(convert);
 
         if (originF != 'ERROR' && convertF != 'ERROR') {
-            await fetch('https://api.cambio.today/v1/quotes/EUR/USD/json?quantity=1&key=14890|sDTD7WGRpU_gnUW~93Cfqpd~gZnWmqMq')
-                .then(async response => await console.log(response.json()))
+
+            //URL
+            const url = `https://xecdapi.xe.com/v1/convert_to.json/?to=${originF}&from=${convertF}&amount=${coinTo}`;
+
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url);
+
+            //Auth
+            xhr.setRequestHeader("Authorization", "Basic dW5pdmVyc2lkYWR0ZWNub2xvZ2ljYWRlbG5vcnRlZGVndWFuYWp1YXRvMTE0NzIxMzQ3OmdoczNzbTNrZTFqZG92NnRzM212ZzdmN2gy");
+
+            let jsonRes;
+            xhr.onreadystatechange = async function () {
+                if (xhr.readyState === 4) {
+                    jsonRes = await JSON.parse(xhr.responseText);
+                    console.log(jsonRes);
+                    document.getElementById('coinFrom').value = jsonRes.from[0].mid;
+                }
+            };
+
+            xhr.send();
+
         } else {
             alert("Uso inadecuado");
         }
@@ -19,11 +41,21 @@ async function validate() {
     }
 }
 
-function valueFormat(value){
-    switch (value){
-        case '1': return 'MXN'; break;
-        case '2': return 'EUR'; break;
-        case '3': return 'BTC'; break;
-        default: return 'ERROR';
+function valueFormat(value) {
+    switch (value) {
+        case '1':
+            return 'MXN';
+            break;
+        case '2':
+            return 'EUR';
+            break;
+        case '3':
+            return 'GBP';
+            break;
+        case '4':
+            return 'USD';
+            break;
+        default:
+            return 'ERROR';
     }
 }
